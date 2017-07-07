73b909ffd1ac9d5d4998df955d9f05d24085e8ee
Edgar Mantes <edgar.mantes11@gmail.com>
2017-07-07 00:06:43 -0500

    Started with Forecast Dashboard components

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<Router history={hashHistory}> 
+			<Route path='/' component={App} />
+		</Router>
+	</Provider>
+, document.querySelector('.container'));
